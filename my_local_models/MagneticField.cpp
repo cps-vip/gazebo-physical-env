7a051e1d0eb05b69af8eaa5383539e197bb824da
josephmasson26 <jmasson6@gatech.edu>
2024-09-20 13:20:08 -0400

    before make

--- my_local_models/MagneticField.cpp
+++ my_local_models/MagneticField.cpp
@@ -1,1 +1,36 @@
 #include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+
+//based on examples from https://github.com/srmainwaring
+
+
+namespace gazebo
+{
+  class MagneticFieldPlugin : public ModelPlugin
+  {
+  public:
+    void Load(physics::ModelPtr _model, sdf::ElementPtr /*_sdf*/)
+    {
+      ignition::math::Vector3d magneticField(0, 0, 0.00005); //TODO changeme later
+      this->model = _model;
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          std::bind(&MagneticFieldPlugin::OnUpdate, this));
+    }
+
+    void OnUpdate()
+    {
+      // calc and apply magnetic force
+      ignition::math::Vector3d force = magneticField * this->model->WorldPose().Pos(); 
+      this->model->GetLink()->AddForce(force);
+      // should add to model
+    }
+
+  private:
+    physics::ModelPtr model;
+    event::ConnectionPtr updateConnection;
+    ignition::math::Vector3d magneticField; //gz behavior
+  };
+
+  GZ_REGISTER_MODEL_PLUGIN(MagneticFieldPlugin)
+}
